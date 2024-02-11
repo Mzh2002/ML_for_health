333a459f7c28bd361e0341314c644e14d33d02a1
mzh <zm4hy@virginia.edu>
2024-02-10 20:40:50 -0500

    modified documentation

--- get_index/get_affordability_index.py
+++ get_index/get_affordability_index.py
@@ -1,10 +1,10 @@
 def get_affordability_index(housing_cost, transportation_cost, total_income):
     """
-    :param housing_cost: (float) the total cost of housing in an area
-    :param transportation_cost: (float) the total cost of transportation in an area
-    :param total_income: (float) the total income in an area
+    :param (float) housing_cost: the total cost of housing in an area
+    :param (float) transportation_cost: the total cost of transportation in an area
+    :param (float) total_income: the total income in an area
 
-    :return (float) affordability index, used to measure the proportion of income spent on housing
+    :return (float): affordability index, used to measure the proportion of income spent on housing
 and transportation
     """
     return (housing_cost + transportation_cost) / total_income
